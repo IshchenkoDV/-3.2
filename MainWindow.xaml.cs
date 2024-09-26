3ce78f9f43a4e954460fe2aa3d9ea61798557ef7
IschenkoDV <IschenkoDV@310D-12>
2024-09-26 12:16:37 +0700

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,156 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        private Point? startPoint;
+        private Point? endPoint;
+        private Color selectedColor;
+        private double brushSize;
+        private DrawingMode currentMode;
+
+        public enum DrawingMode
+        {
+            Draw,
+            Edit,
+            Erase
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            selectedColor = Colors.Black;
+            brushSize = 1;
+            currentMode = DrawingMode.Draw;
+            ColorPicker.ItemsSource = new List<Color>
+            {
+                Colors.Black, Colors.Red, Colors.Green, Colors.Blue, Colors.Yellow, Colors.Magenta, Colors.Cyan
+            };
+            ColorPicker.SelectedIndex = 0;
+
+            BrushSizeSlider.Minimum = 1;
+            BrushSizeSlider.Maximum = 100;
+            BrushSizeSlider.Value = brushSize;
+
+            DrawRadioButton.IsChecked = true;
+        }
+
+        private void ColorPicker_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            selectedColor = (Color)ColorPicker.SelectedItem;
+        }
+
+        private void BrushSizeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            brushSize = BrushSizeSlider.Value;
+        }
+
+        private void ModeRadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (sender == DrawRadioButton)
+            {
+                currentMode = DrawingMode.Draw;
+            }
+            else if (sender == EditRadioButton)
+            {
+                currentMode = DrawingMode.Edit;
+            }
+            else if (sender == EraseRadioButton)
+            {
+                currentMode = DrawingMode.Erase;
+            }
+        }
+
+        private void Canvas_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            startPoint = e.GetPosition(Canvas);
+            endPoint = startPoint;
+
+            if (currentMode == DrawingMode.Draw)
+            {
+                DrawLine(startPoint, endPoint, selectedColor, brushSize);
+            }
+            else if (currentMode == DrawingMode.Erase)
+            {
+                EraseLineAtPoint(startPoint);
+            }
+        }
+
+        private void Canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed && startPoint.HasValue)
+            {
+                endPoint = e.GetPosition(Canvas);
+
+                if (currentMode == DrawingMode.Draw)
+                {
+                    DrawLine(startPoint, endPoint, selectedColor, brushSize);
+                }
+                else if (currentMode == DrawingMode.Edit)
+                {
+                    DrawLine(startPoint, endPoint, Colors.White, brushSize);
+                    
+                }
+
+                startPoint = endPoint;
+            }
+        }
+
+        private void Canvas_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            startPoint = null;
+        }
+
+        private void DrawLine(Point? startPoint, Point? endPoint, Color color, double thickness)
+        {
+            if (startPoint.HasValue && endPoint.HasValue)
+            {
+                Line line = new Line
+                {
+                    Stroke = new SolidColorBrush(color),
+                    StrokeThickness = thickness,
+                    X1 = startPoint.Value.X,
+                    Y1 = startPoint.Value.Y,
+                    X2 = endPoint.Value.X,
+                    Y2 = endPoint.Value.Y
+
+                };
+                Canvas.Children.Add(line);
+            }
+        }
+
+        private void EraseLineAtPoint(Point? point)
+        {
+            if (point.HasValue)
+            {
+                var lineToRemove = FindLineAtPoint(point.Value);
+                if (lineToRemove != null)
+                {
+                    Canvas.Children.Remove(lineToRemove);
+                }
+            }
+        }
+
+        private Line FindLineAtPoint(Point point)
+        {
+            foreach (UIElement element in Canvas.Children)
+            {
+                if (element is Line line && line.IsMouseOver)
+                {
+                    return line;
+                }
+            }
+            return null;
+        }
+    }
+}
